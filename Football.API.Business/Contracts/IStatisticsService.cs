b50d60bfff97fb278ca2f4cdd84e96e3aa3231e6
Alejandro Bolaño <elbola.cuba1992@gmail.com>
2023-02-08 23:43:30 +0100

    feat: Add new services about statistics

---
+++ Football.API.Business/Contracts/IStatisticsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football.API.Business.Contracts
+{
+    internal interface IStatisticsService
+    {
+        public int GetYellowCards();
+        public int GetRedCards();
+    }
+}
---
+++ Football.API.Business/Implementations/StatisticsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Football.API.Business.Contracts;
+
+namespace Football.API.Business.Implementations
+{
+    public class StatisticsService: IStatisticsService
+    {
+        public int GetYellowCards()
+        {
+            return 2;
+        }
+
+        public int GetRedCards()
+        {
+            return 1;
+        }
+    }
+}
