30050e65c4a05284c27a4508b92eb8baaad04814
Alejandro Bolaño <elbola.cuba1992@gmail.com>
2023-02-08 23:40:12 +0100

    feat: Adjust new classes with access to Repository.
    Add wrapper and di

---
+++ Football.API.DataAccess/Contracts/IManagerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Football.API.Common.Models;
+
+namespace Football.API.DataAccess.Contracts
+{
+    public interface IManagerRepository : IRepositoryBase<ManagerResponse>
+    {
+        Task<IEnumerable<ManagerResponse>> GetAllAsync();
+        Task<ManagerResponse> GetByIdAsync(int id);
+    }
+}
---
+++ Football.API.DataAccess/Implementations/ManagerRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Football.API.Common.Models;
+using Football.API.DataAccess.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Football.API.DataAccess.Implementations
+{
+    public class ManagerRepository: RepositoryBase<ManagerResponse>, IManagerRepository
+    {
+        public ManagerRepository(FootballContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public async Task<IEnumerable<ManagerResponse>> GetAllAsync() => await GetAll().OrderBy(manager => manager.Name).ToListAsync();
+
+        public async Task<ManagerResponse> GetByIdAsync(int id) => await FindByCondition(manager => manager.Id.Equals(id)).FirstOrDefaultAsync();
+    }
+}
