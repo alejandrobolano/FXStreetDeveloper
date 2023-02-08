24f758694f382f7d6a667cc901beecb46e03d953
Alejandro Bolaño <elbola.cuba1992@gmail.com>
2023-02-08 23:43:07 +0100

    feat: Improvement ManagerController. Take this controller like example

--- Football.API/Controllers/ManagerController.cs
+++ Football.API/Controllers/ManagerController.cs
@@ -1,6 +1,13 @@
-﻿using Football.API.Models;
+﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Football.API.Common.Contracts;
+using Football.API.Common.Models;
+using Football.API.DataAccess.Contracts;
+using AutoMapper;
+using Football.API.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Football.API.Controllers
 {
@@ -8,45 +15,124 @@ namespace Football.API.Controllers
     [ApiController]
     public class ManagerController : ControllerBase
     {
-        readonly FootballContext footballContext;
-        public ManagerController(FootballContext footballContext)
+        private readonly ILoggerManager _logger;
+        private readonly IRepositoryWrapper _repository;
+        private readonly IMapper _mapper;
+        public ManagerController(ILoggerManager logger, IRepositoryWrapper repository, IMapper mapper)
         {
-            this.footballContext = footballContext;
+            _logger = logger;
+            _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [Route("")]
-        public ActionResult<IEnumerable<Manager>> Get()
+        //TODO All Get's methods can be changed by GetAll(), this way will indicate that it result are lists
+        public async Task<ActionResult<IEnumerable<ManagerResponse>>> Get()
         {
-            return this.Ok(footballContext.Managers);
+            var managers = await _repository.ManagerRepository.GetAllAsync();
+            return Ok(managers);
         }
 
         [HttpGet]
-        [Route("{id}", Name = "GetById")]
-        public ActionResult GetById(int id)
+        [Route("{id}")]
+        public async Task<ActionResult> GetById(int id)
         {
-            var response = footballContext.Managers.Find(id);
+            var response = await _repository.ManagerRepository.GetByIdAsync(id);
             if (response == default)
-                this.NotFound();
-            return this.Ok();
+                return NotFound();
+            return Ok(response);
         }
 
         [HttpPost]
-        public ActionResult Post(Manager manager)
+        //TODO Change Post by Add
+        public async Task<ActionResult> Add(ManagerRequest managerRequest)
         {
-            var response = footballContext.Managers.Add(manager).Entity;
-            return this.CreatedAtAction("GetById", response.Id, response);
+            var manager = _mapper.Map<ManagerResponse>(managerRequest);
+            var response = _repository.ManagerRepository.Add(manager);
+            try
+            {
+                await _repository.SaveAsync();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                var className = GetType().Name;
+                var errorLine = new System.Diagnostics.StackFrame(0, true).GetFileLineNumber();
+                _logger.LogError($"Something went wrong inside the Update action. {dbUpdateException.Message}", className, errorLine);
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                var className = GetType().Name;
+                var errorLine = new System.Diagnostics.StackFrame(0, true).GetFileLineNumber();
+                _logger.LogError($"Something went wrong inside the Update action. {exception.Message}", className, errorLine);
+                return StatusCode(500, "Internal server error");
+            }
+            
+            return CreatedAtRoute(response.Id, response);
         }
 
         [HttpPut]
         [Route("{id}")]
-        public ActionResult Update(int id, Manager manager)
+        public async Task<ActionResult> Update(int id, [FromBody]ManagerRequest managerRequest)
         {
-            if (footballContext.Managers.Find(id) == default)
-                return this.NotFound();
+            var managerMapped = _mapper.Map<ManagerResponse>(managerRequest);
+            var manager = await _repository.ManagerRepository.GetByIdAsync(id);
+            if (manager == default)
+                return NotFound();
+            try
+            {
+                managerMapped.Id = manager.Id;
+                _repository.ManagerRepository.Update(managerMapped);
+                await _repository.SaveAsync();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                var className = GetType().Name;
+                var errorLine = new System.Diagnostics.StackFrame(0, true).GetFileLineNumber();
+                _logger.LogError($"Something went wrong inside the Update action. {dbUpdateException.Message}", className, errorLine);
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                var className = GetType().Name;
+                var errorLine = new System.Diagnostics.StackFrame(0, true).GetFileLineNumber();
+                _logger.LogError($"Something went wrong inside the Update action. {exception.Message}", className, errorLine);
+                return StatusCode(500, "Internal server error");
+            }
 
-            footballContext.Managers.Update(manager);           
-            return this.Ok();
+
+            return Ok(managerMapped);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var manager = await _repository.ManagerRepository.GetByIdAsync(id);
+            if (manager == default)
+                return NotFound();
+            try
+            {
+                _repository.ManagerRepository.Delete(manager);
+                await _repository.SaveAsync();
+            }
+
+            catch (DbUpdateException dbUpdateException)
+            {
+                var className = GetType().Name;
+                var errorLine = new System.Diagnostics.StackFrame(0, true).GetFileLineNumber();
+                _logger.LogError($"Something went wrong inside the Delete action. {dbUpdateException.Message}", className, errorLine);
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                var className = GetType().Name;
+                var errorLine = new System.Diagnostics.StackFrame(0, true).GetFileLineNumber();
+                _logger.LogError($"Something went wrong inside the Delete action. {exception.Message}", className, errorLine);
+                return StatusCode(500, "Internal server error");
+            }
+            return Ok("Delete successful");
         }
     }
 }
